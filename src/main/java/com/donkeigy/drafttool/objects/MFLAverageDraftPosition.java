0fd174a7f18f28debc1cc5c01c54e1a0b9ffe045
Cedric Charles <cjcharles777@gmail.com>
2014-08-13 13:41:26 -0500

    Initial commit

---
+++ src/main/java/com/donkeigy/drafttool/objects/MFLAverageDraftPosition.java
@@ -0,0 +1,53 @@
+package com.donkeigy.drafttool.objects;
+
+/**
+ * Created by cedric on 8/13/14.
+ */
+public class MFLAverageDraftPosition
+{
+    String  minPick;
+    String  maxPick;
+    String draftsSelectedIn;
+    String id;
+    String averagePick;
+
+    public String getMinPick() {
+        return minPick;
+    }
+
+    public void setMinPick(String minPick) {
+        this.minPick = minPick;
+    }
+
+    public String getMaxPick() {
+        return maxPick;
+    }
+
+    public void setMaxPick(String maxPick) {
+        this.maxPick = maxPick;
+    }
+
+    public String getDraftsSelectedIn() {
+        return draftsSelectedIn;
+    }
+
+    public void setDraftsSelectedIn(String draftsSelectedIn) {
+        this.draftsSelectedIn = draftsSelectedIn;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getAveragePick() {
+        return averagePick;
+    }
+
+    public void setAveragePick(String averagePick) {
+        this.averagePick = averagePick;
+    }
+}
---
+++ src/main/java/com/donkeigy/drafttool/objects/MFLPlayer.java
@@ -0,0 +1,55 @@
+package com.donkeigy.drafttool.objects;
+
+/**
+ * Created by cedric on 8/13/14.
+ */
+public class MFLPlayer
+{
+    String position;
+    String name;
+    String id;
+    String team;
+    String twitterUsername;
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTeam() {
+        return team;
+    }
+
+    public void setTeam(String team) {
+        this.team = team;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTwitterUsername()
+    {
+        return twitterUsername;
+    }
+
+    public void setTwitterUsername(String twitterUsername)
+    {
+        this.twitterUsername = twitterUsername;
+    }
+}
